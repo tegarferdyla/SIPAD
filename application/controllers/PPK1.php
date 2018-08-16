c1a492281cc8c8b892b54764defc40f40cf9bad4
dalas98 <dalas98@gmail.com>
2018-08-17 03:08:25 +0700

    update task

--- application/controllers/PPK1.php
+++ application/controllers/PPK1.php
@@ -91,6 +91,63 @@ class PPK1 extends CI_Controller {
 		$this->load->view('ppk1/header', $data);
 
 		$id_ppk = $this->session->userdata('id_ppk');
+		$tahun =  date('Y');
+		$cetaktahun = $this->Datatahun_model->cektahunppk($tahun,$id_ppk);
+		$id_tahun = $cetaktahun->id_tahun;
+		$countdata['tbl_doc1'] = $this->Datapaket_model->countonme($id_tahun);
+		$countdata['tbl_doc2'] = $this->Datapaket_model->countdoc2($id_tahun);
+		$countdata['tbl_doc3'] = $this->Datapaket_model->countdoc3($id_tahun);
+		$countdata['tbl_addendumii'] = $this->Datapaket_model->countdoc4($id_tahun);
+		$countdata['tbl_addendumiii'] = $this->Datapaket_model->countdoc5($id_tahun);
+		$countdata['tbl_addendumiv'] = $this->Datapaket_model->countdoc6($id_tahun);
+		$countdata['tbl_pascamc0'] = $this->Datapaket_model->countdoc7($id_tahun);
+		$countdata['tbl_pendukung'] = $this->Datapaket_model->countdoc8($id_tahun);
+		$data['tahun'] = $this->Datatahun_model->cektahun($id_tahun);
+		$id_ppk = $data['tahun']->id_ppk;
+		$data['get_tahun'] = $this->Datatahun_model->datatahun($id_ppk);
+
+		$nama_paket = $this->Datapaket_model->lihatpaket($id_tahun);
+		for($i=0;$i<count($nama_paket);$i++){
+			$nama = $nama_paket[$i]['nama_paket'];
+			$jenis = $nama_paket[$i]['jenis'];
+			$num_doc1 = $countdata['tbl_doc1'][$i]->hasil;
+			$num_doc2 = $countdata['tbl_doc2'][$i]->hasil;
+			$num_doc3 = $countdata['tbl_doc3'][$i]->hasil;
+			$num_doc4 = $countdata['tbl_pascamc0'][$i]->hasil;
+			$num_doc5 = $countdata['tbl_addendumii'][$i]->hasil;
+			$num_doc6 = $countdata['tbl_addendumiii'][$i]->hasil;
+			$num_doc7 = $countdata['tbl_addendumiv'][$i]->hasil;
+			if ($countdata['tbl_pendukung']==NULL) {
+				$pendukung = 0;
+			}else{
+				$pendukung = $countdata['tbl_pendukung'][$i]->hasil;
+			}
+			if ($countdata['tbl_addendumii'][$i]->hasil == 0) {
+				$adendum2 = 0;
+			}else{
+				$adendum2 = 8;
+			}
+
+			if ($countdata['tbl_addendumiii'][$i]->hasil == 0) {
+				$adendum3 = 0;
+			}else{
+				$adendum3 = 8;
+			}
+
+			if ($countdata['tbl_addendumiv'][$i]->hasil == 0) {
+				$adendum4 = 0;
+			}else{
+				$adendum4 = 8;
+			}
+
+			$num1 = $num_doc1+$num_doc2+$num_doc3+$num_doc4+$pendukung+$num_doc5+$num_doc6+$num_doc7;
+			$num2 = 53+$adendum2+$adendum3+$adendum4;
+			$hitungan = ($num1/$num2)*100;
+			$hasil[]['total'] = number_format($hitungan,1);
+			$json[] = array('nama_paket'=> $nama,'jenis'=>$jenis,'paket_terkumpul' => $num1, 'total' => $hasil[$i]['total']);
+		}
+		$cetak = json_encode($json);
+		$data['hasil'] = json_decode($cetak);
 		$ppk = $this->Datappk_model->GetWherePPK("where id_ppk ='$id_ppk'");
 		$ppk = array("nama" => $ppk[0]['nama']);
 		$this->load->view('ppk1/sidebar', $ppk);
--- application/views/ppk1/footer.php
+++ application/views/ppk1/footer.php
@@ -3,7 +3,6 @@
 </div>
     <!--/ #wrapper -->
     <!-- Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.2/umd/popper.min.js"></script>
     <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/2.7.0/metisMenu.min.js"></script>
@@ -11,7 +10,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.15/js/jquery.dataTables.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-footable/3.1.4/footable.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script>
     <script src="assets/vendors/charts/utils.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/countup.js/1.9.2/countUp.min.js"></script>
